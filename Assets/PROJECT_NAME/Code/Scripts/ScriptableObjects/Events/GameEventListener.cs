e34074dfd9d22b08070aa915a3fa978697df5904
Joshua Weinstein <Joshua.E.Weinstein@gmail.com>
2023-07-07 02:09:38 -0400

    First Commit

---
+++ Assets/PROJECT_NAME/Code/Scripts/ScriptableObjects/Events/GameEventListener.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace TemporaryGameCompany
+{
+    public class GameEventListener : MonoBehaviour
+    {
+        [Tooltip("Event registered with.")]
+        public GameEvent Event;
+        [Tooltip("Response invoked when event raised.")]
+        public UnityEvent Response;
+
+        private void OnEnable()
+        {
+            Event.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            Event.UnregisterListener(this);
+        }
+
+        public void OnEventRaised()
+        {
+            if (this.enabled)
+            {
+                Response.Invoke();
+            }
+        }
+    }
+}
---
+++ Assets/PROJECT_NAME/Code/Scripts/ScriptableObjects/Variables/FloatVariable.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public delegate void ValueChangedDelegate(float oldValue, float newValue);
+namespace TemporaryGameCompany
+{
+    [CreateAssetMenu(menuName = "Variables/FloatVariable")]
+    public class FloatVariable : ScriptableObject
+    {
+        #if UNITY_EDITOR
+        [Multiline] public string DeveloperDescription = "";
+        #endif
+
+        public float Value; // Value of float.
+
+        public ValueChangedDelegate ValueChanged;
+
+        // Set value.
+        public void SetValue(float value) 
+        { 
+            float old = Value;
+            Value = value; 
+            ValueChanged(old, Value);
+        }
+
+        public void SetValue(FloatVariable value)
+        {
+            float old = Value;
+            Value = value.Value;
+            ValueChanged(old, Value);
+
+        }
+
+        public void ApplyChange(float amount)
+        {
+            float old = Value;
+            Value += amount;
+            ValueChanged(old, Value);
+        }
+
+        public void ApplyChange(FloatVariable amount)
+        {
+            float old = Value;
+            Value += amount.Value;
+            ValueChanged(old, Value);
+        }
+    }
+}
